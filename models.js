18aa64ef1665d3da33949c8a8085370dbb0b9670
Kingsford-Q <158656114+Kingsford-Q@users.noreply.github.com>
2025-03-05 17:24:25 +0000

    Changes

--- models.js
+++
@@ -1,8 +0,0 @@
-const connectDB = require("./db");
-
-async function getBooksCollection() {
-    const db = await connectDB();
-    return db.collection("books");
-}
-
-module.exports = getBooksCollection;
